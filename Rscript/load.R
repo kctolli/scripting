802fc1a1b3024b834a3363f2c1ccf73683dcd50b
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-11 20:00:26 -0700

    load

---
+++ Rscript/load.R
@@ -0,0 +1,11 @@
+## Load Functions
+
+load_libraries <- function(){
+  p_load(tidyverse, glue, pander, lubridate, knitr, rmarkdown) ## Load Packages
+  opts_chunk$set(results = 'asis', echo = FALSE, message = FALSE, warning = FALSE) ## Chunk Displays
+}
+
+# Variables
+
+here <- here::here() ## set here file path
+current_year <- lubridate::year(Sys.Date()) ## sys year 
