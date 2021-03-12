2b079de1759dea89a114ebc5105d6fe0f0862e18
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-11 21:15:09 -0700

    copyright

--- Rscript/load.R
+++ Rscript/load.R
@@ -1,4 +1,4 @@
-## Load Functions
+# Load Functions
 
 load_libraries <- function(){
   p_load(tidyverse, glue, pander, lubridate, knitr, rmarkdown) ## Load Packages
@@ -9,3 +9,13 @@ load_libraries <- function(){
 
 here <- here::here() ## set here file path
 current_year <- lubridate::year(Sys.Date()) ## sys year 
+
+# Copyright util
+
+copyright <- function(start){
+  if (start <= current_year){
+    if (current_year != start) {str <- glue::glue('{start} - {current_year} -- Kyle Tolliver')}
+    else {str <- glue::glue('{current_year} -- Kyle Tolliver')}}
+  else {str <- 'Time Error'}
+  pander::pander(glue::glue('<i class="far fa-copyright"></i>{str}'))
+}
