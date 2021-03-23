949ac17b6c25ba5b678840ebf6c118a457179111
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-23 13:12:34 -0600

    current year

--- Rscript/copyright.R
+++ Rscript/copyright.R
@@ -1,5 +1,6 @@
 # copyright
 copyright <- function(start){
+  current_year <- lubridate::year(Sys.Date()) ## sys year
   if (start < current_year){str <- glue::glue('{start} - {current_year} -- Kyle Tolliver')}
   else if (start == current_year) {str <- glue::glue('{current_year} -- Kyle Tolliver')}
   else {str <- 'Time Error'}
