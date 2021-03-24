c4d7d3ccd93a89c53ecbf44cacd5f4a164026738
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-23 21:24:28 -0600

    basic

--- Rscript/templates.R
+++ Rscript/templates.R
@@ -13,3 +13,48 @@ print_solo <- function(){print(glue::glue_data(solo, "- {name} - [#{number}](htt
 print_soc <- function(){print(glue::glue_data(society, "- {group} associated with {loc} ({start} - {end}) \n"))}
 print_highlights <- function(){print(glue::glue_data(highlights, "- [{Text}]({Link}) \n"))}
 print_portfolio <- function(){pander::pander('<p class="info">This website is setup as a personal portfolio.</p>')}
+
+print_tutor <- function(){
+  string <- "javascript:showhide('tutor')"
+  
+  pander::pander(glue::glue('
+  ### Computer Science Tutor
+
+  April 2018 - August 2018 --- Rexburg, ID
+
+  <ul><div style="padding-left:0px;">
+  <span class="tooltipr"><a href={string}><li>Tutored many classes</li></a></span>
+  <div id="tutor" style="display:none;padding-left:20px;">
+
+    + Object-Oriented Programming with Data Structures using Python
+    + Fundamentals of Digital Systems w/ Lab
+    + Introduction to Engineering
+    + Introduction to Electrical and Computer Engineering
+
+  </div></div><li>Taught students topics from these classes</li>
+  <li>Lead group and individual appointments and discussions</li></ul>'))
+}
+
+print_ds <- function(){
+  string <- "javascript:showhide('pic')"
+  
+  pander::pander(glue::glue('
+  <div style="padding-left:0px;"><span class="tooltipr"><a href={string} align="center">
+  <img src="https://secure.meetupstatic.com/photos/event/d/7/4/b/600_482695115.jpeg" 
+  alt="Data Science Image" class="img"></a></span>
+  <h4 id="pic" style="display:none;padding-left:20px;">Interdisciplinary field that uses scientific methods, 
+  processes, algorithms and systems to extract knowledge and insights from structured and unstructured data, 
+  and apply knowledge and actionable insights from data across a broad range of application domains.</h4></div>\n\n\n'))
+}
+
+print_about <- function(){
+  # pic <- print_pic("./site_libs/images/me.jpg")
+  string <- "javascript:showhide('me')"
+  
+  pander::pander(glue::glue('## About Me \n\n 
+  BYU-Idaho graduate with a Bachelor\'s (BS) in Software Engineering. 
+  I also earned a minor in Computer Engineering and Data Science. 
+  I love to learn new things and solve problems. \n\n
+  <div style="padding-left:0px;"><span class="tooltipr"><a href={string}>
+  A little more about me</a></span><div id="me" style="display:none;padding-left:20px;">\n\n'))
+}
