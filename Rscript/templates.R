7b4876aa4c934f5d936b0fae742ae1ae7ff95aac
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-23 21:20:21 -0600

    templates

--- Rscript/templates.R
+++ Rscript/templates.R
@@ -6,4 +6,10 @@ print_contact <- function(file){
   
   print(glue::glue_data(info, "<i class='fa fa-{icon}'></i> [{contact}]({link}) \n\n"))
 }
+
 print_skills <- function(){print(glue::glue_data(skills, "- {skill} \n"))}
+print_pos <- function(section_id){section(pos, section_id, "- {name}\n")}
+print_solo <- function(){print(glue::glue_data(solo, "- {name} - [#{number}](https://www.sololearn.com/{link}/pdf)\n"))}
+print_soc <- function(){print(glue::glue_data(society, "- {group} associated with {loc} ({start} - {end}) \n"))}
+print_highlights <- function(){print(glue::glue_data(highlights, "- [{Text}]({Link}) \n"))}
+print_portfolio <- function(){pander::pander('<p class="info">This website is setup as a personal portfolio.</p>')}
