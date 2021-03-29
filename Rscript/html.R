6d570b3b54f2326e7917639672d46bf11726018c
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-29 17:46:53 -0600

    pander

--- Rscript/html.R
+++ Rscript/html.R
@@ -1,26 +1,27 @@
 library(htmltools)
 library(htmlwidgets)
 
-library(knitr, rmarkdown)
+library(knitr, rmarkdown, pander, glue)
 
 opts_chunk$set(results = 'asis', echo = FALSE, message = FALSE, warning = FALSE) ## Chunk Displays
 
 # R to HTML Functions
 
 print_newline <- function(){pander("\n")}
-print_h1 <- function(h1){pander::pander(glue::glue("# {h1} \n\n\n"))}
-print_h2 <- function(h2){pander::pander(glue::glue("## {h2} \n\n\n"))}
-print_h3 <- function(h3){pander::pander(glue::glue("### {h3} \n\n\n"))}
-print_h4 <- function(h4){pander::pander(glue::glue("#### {h4} \n\n\n"))}
-print_h5 <- function(h5){pander::pander(glue::glue("##### {h5} \n\n\n"))}
-print_h6 <- function(h6){pander::pander(glue::glue("###### {h6} \n\n\n"))}
-print_pic <- function(img){pander::pander(glue::glue("![]({img}) \n\n\n"))}
-print_strong <- function(p){pander::pander(glue::glue('<strong>{p}</strong>'))} 
-pagebreak <- function(){pander::pander('<hr /><div style="clear:both;"></div>')}
+print_h1 <- function(h1){pander(glue("# {h1} \n\n\n"))}
+print_h2 <- function(h2){pander(glue("## {h2} \n\n\n"))}
+print_h3 <- function(h3){pander(glue("### {h3} \n\n\n"))}
+print_h4 <- function(h4){pander(glue("#### {h4} \n\n\n"))}
+print_h5 <- function(h5){pander(glue("##### {h5} \n\n\n"))}
+print_h6 <- function(h6){pander(glue("###### {h6} \n\n\n"))}
+print_pic <- function(img){pander(glue("![]({img}) \n\n\n"))}
+print_strong <- function(p){pander(glue('<strong>{p}</strong>'))} 
+pagebreak <- function(){pander('<hr /><div style="clear:both;"></div>')}
+divbreak <- function(){pander('</div></div><br /> \n\n\n')}
 
 # R Styles
 
-gray <- function(){pander::pander('style="color:gray;"')}
-white <- function(){pander::pander('style="color:white;"')}
-px0 <- function(){pander::pander('style="padding-left:0px;"')}
-px20 <- function(){pander::pander('style="display:none;padding-left:20px;"')}
+gray <- function(){pander('style="color:gray;"')}
+white <- function(){pander('style="color:white;"')}
+px0 <- function(){pander('style="padding-left:0px;"')}
+px20 <- function(){pander('style="display:none;padding-left:20px;"')}
