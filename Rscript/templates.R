47682a21c6d4e9ce7bda79a6c67b639a6e1cf57b
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-30 15:32:18 -0600

    temp

--- Rscript/templates.R
+++ Rscript/templates.R
@@ -1,27 +1,11 @@
+source('./basictemplates.R')
+source('./skilltemplates.R')
+
 library(glue)
 library(pander)
 library(tidyverse)
 
-here <- here::here() ## set here file path
-
 section <- function(cv, section_id, glue_template){print(glue_data(filter(cv, section == section_id), glue_template))}
-
-print_contact <- function(file){
-  
-  if (file == "resume"){info <- filter(contact, in_resume)}
-  else if (file == "index"){info <- filter(contact, in_index)}
-  else {info <- contact}
-  
-  print(glue_data(info, "<i class='fa fa-{icon}'></i> [{contact}]({link}) \n\n"))
-}
-
-print_skills <- function(){print(glue_data(dplyr::filter(skills, level >= 5), "- {skill} \n"))}
-print_hobbies <- function(){print(glue_data(hobbies, "- {hobby} \n"))}
-print_pos <- function(section_id){section(pos, section_id, "- {name}\n")}
-print_solo <- function(){print(glue_data(solo, "- {name} - [#{number}](https://www.sololearn.com/{link}/pdf)\n"))}
-print_soc <- function(){print(glue_data(society, "- {group} associated with {loc} ({start} - {end}) \n"))}
-print_highlights <- function(){print(glue_data(highlights, "- [{Text}]({Link}) \n"))}
-print_portfolio <- function(){pander('<p class="info">This website is setup as a personal portfolio.</p>')}
 
 print_tutor <- function(){
   pander(glue('
