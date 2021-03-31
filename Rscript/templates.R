edef018d8985fc38286e6df3580c0a6e941b08e7
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-31 01:31:23 -0600

    link

--- Rscript/templates.R
+++ Rscript/templates.R
@@ -87,7 +87,7 @@ print_byuiwork <- function(){
 print_readme <- function(){
   pander('
   * [Portfolio](https://kctolli.github.io/)
-    + [Resume](https://kctolli.github.io/site_libs/resume/resume.html)
+    + [Resume](https://kctolli.github.io/Resume/)
     + [Diploma](https://kctolli.github.io/site_libs/images/diploma.pdf) \n\n\n
   <h2>User Stats</h2><a href="https://github.com/anuraghazra/github-readme-stats" align="center"><img style="max-width:100%;"
   height="160" align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=kctolli&layout=compact&theme=gruvbox"/></a> 
