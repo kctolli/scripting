65d8e4b73c023f576c3fff019f585bb9e328cf4d
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-31 01:14:43 -0600

    include

---
+++ Rscript/include.R
@@ -0,0 +1,9 @@
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/js.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/css.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/html.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/gsheets.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/copyright.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/templates.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/skills.R")
+
+library(pander, glue, tidyverse)
