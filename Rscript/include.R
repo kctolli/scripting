1f69e82b8117f296a1fda4b20235434e0954e602
kctolli <kyle@tollivers.org>
2021-04-20 00:11:13 -0600

    Update at 00:11 on 04/20/2021

--- Rscript/include.R
+++ Rscript/include.R
@@ -4,6 +4,7 @@ source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/html.R"
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/gsheets.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/copyright.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/templates.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/basictemplates.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/skills.R")
 
 library(pander, glue, tidyverse)
